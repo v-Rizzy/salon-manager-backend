838e92d920ca413f78f076679112cf3484b56672
v-rizzy <rzwnzbr@gmail.com>
2024-11-15 22:42:21 +0530

    fix: use env vars without dotenv

--- preload.js
+++ preload.js
@@ -1,23 +1,9 @@
 /* eslint-disable */
-
-const dotenv = require('dotenv');
 const path = require('path');
 const moduleAlias = require('module-alias');
 
-
-// Check the env
-const NODE_ENV = (process.env.NODE_ENV || 'development');
-
-// Configure "dotenv"
-const result2 = dotenv.config({
-  path: path.join(__dirname, `./env/${NODE_ENV}.env`),
-});
-if (result2.error) {
-  throw result2.error;
-}
-
-// Configure module-alias
-if (NODE_ENV === 'production') {
-  moduleAlias.addAlias('@src', __dirname + '/dist');
+// Configure module-alias for production
+if (process.env.NODE_ENV === 'production') {
+  moduleAlias.addAlias('@src', path.join(__dirname, 'dist'));
 }
 
